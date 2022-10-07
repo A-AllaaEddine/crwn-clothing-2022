08e0ce13fb15da36f71bc73fad04e45da0db40ba
A-AllaaEddine <a.allaaeddine.riad@gmail.com>
2022-10-07 09:13:37 +0100

    created checkout page along with checkout items and checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,10 +4,19 @@ import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
 
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 
 const CartDropdown = () => {
-    const {cartItems } = useContext(CartContext);
+    const {cartItems, isCartOpen, setIsCartOpen, } = useContext(CartContext);
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+        toggleIsCartOpen();
+    }
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -15,7 +24,7 @@ const CartDropdown = () => {
                 <CartItem key={item.id} cartItem={item} />
                 ))}
             </div>
-            <Button >GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler} >GO TO CHECKOUT</Button>
         </div>
 
     )
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,39 @@
+import './checkout-item.styles.scss';
+
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
+const CheckoutItem = ({ cartItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    
+    const { clearItemFromCart, removeItemToCart, addItemToCart } = useContext(CartContext);
+
+    const clearItemhandle = () => clearItemFromCart(cartItem)
+    const addItemhandle = () => addItemToCart(cartItem)
+    const removeItemhandle = () => removeItemToCart(cartItem)
+
+    return (
+        <div className="checkout-item-container">
+            <div className="image-container">
+                <img src={`${imageUrl}`} alt={`${name}`} />
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>
+                <div className="arrow" onClick={removeItemhandle}>
+                    &#10094;
+                </div>
+                <span className='value'>
+                    {quantity}
+                </span>
+                <div className="arrow" onClick={addItemhandle}>
+                    &#10095;
+                </div>
+            </span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={clearItemhandle}>&#10005;</div>
+        </div>
+    )
+
+}
+
+export default CheckoutItem;
